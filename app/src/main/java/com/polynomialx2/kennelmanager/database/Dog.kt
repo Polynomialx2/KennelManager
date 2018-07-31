0d66a1f124eda4c8802153a2f0e97d2f645fa701
Jason Felton <jasonfelton@MacBook.local>
2018-07-31 19:26:34 -0400

    Add DogDao

--- app/src/main/java/com/polynomialx2/kennelmanager/database/Dog.kt
+++ app/src/main/java/com/polynomialx2/kennelmanager/database/Dog.kt
@@ -1,9 +1,6 @@
 package com.polynomialx2.kennelmanager.database
 
-import android.arch.persistence.room.ColumnInfo
-import android.arch.persistence.room.Entity
-import android.arch.persistence.room.ForeignKey
-import android.arch.persistence.room.PrimaryKey
+import android.arch.persistence.room.*
 import android.support.annotation.NonNull
 
 @Entity(
---
+++ app/src/main/java/com/polynomialx2/kennelmanager/database/DogDao.kt
@@ -0,0 +1,21 @@
+package com.polynomialx2.kennelmanager.database
+
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.Query
+
+interface DogDao {
+
+    @Insert
+    fun insert(dog: Dog)
+
+    @Query("SELECT * FROM Dog WHERE Present = 1 ORDER BY Name ASC")
+    fun getBoardedDogs(): Array<Dog>
+
+    @Query("SELECT * FROM Dog " +
+            "LEFT JOIN Breed " +
+            "ON Dog.BreedID = Breed.BreedID " +
+            "LEFT JOIN Owner " +
+            "ON Dog.OwnerID = Owner.OwnerID " +
+            "ORDER BY Attributes.Name ASC")
+    fun getAllDogs(): Array<Dog>
+}
